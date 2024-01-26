334a6cea6f374d7d83348bf2c9c63d6091e0579a
Maxime Ocafrain <maxime.ocafrain@etu.u-bordeaux.fr>
2024-01-27 00:29:30 +0100

    un peu de logique pour le joueur

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,30 +1,20 @@
 extends CharacterBody2D
 
-
 const speed = 300.0
 const friction = 0.35
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
 func _physics_process(delta):
-	# Once again, we call `Input.get_action_strength()` to support analog movement.
 	var direction := Vector2(
-		# This first line calculates the X direction, the vector's first component.
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
-		# And here, we calculate the Y direction. Note that the Y-axis points 
-		# DOWN in games.
-		# That is to say, a Y value of `1.0` points downward.
 		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	)
-	# When aiming the joystick diagonally, the direction vector can have a length 
-	# greater than 1.0, making the character move faster than our maximum expected
-	# speed. When that happens, we limit the vector's length to ensure the player 
-	# can't go beyond the maximum speed.
 	if direction.length() > 1.0:
 		direction = direction.normalized()
 
 	var target_velocity = direction * speed
 	velocity += (target_velocity - velocity) * friction
 	move_and_slide()
+	
+func _input(event):
+	if event.is_action_pressed("special"):
+		print("YES")
