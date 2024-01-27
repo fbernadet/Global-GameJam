f5be2d7e797e5261e580f4c91a0f6654f8ecee5b
Yanis Dubois <yanis.dubois@etu.u-bordeaux.fr>
2024-01-27 13:12:44 +0100

    plein de truc

--- Scripts/clown.gd
+++ Scripts/clown.gd
@@ -1,18 +1,40 @@
+class_name Clown
 extends CharacterBody2D
 
-var speed = 100
+var speed = 50
 var player: Player = null
-var direction: Vector2
+var direction: Vector2 = Vector2(0, 0)
+var adjacent_clowns = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	player = get_tree().get_nodes_in_group("player")[0]
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	var player_list = get_tree().get_nodes_in_group("player")
+	if not player_list.is_empty():
+		player = player_list[0]
 
 func _physics_process(delta):
-	direction = (player.position - position).normalized()
-	velocity = direction * speed 
-	move_and_slide()
+	var distance = 0
+	if player != null:
+		distance = position.distance_to(player.position)
+		direction = (player.position - position)
+	
+	for c in adjacent_clowns:
+		direction += (position - c.position)
+	direction = direction.normalized()
+	
+	print(distance)
+	if (distance > 8):
+		velocity = direction * speed
+		move_and_slide()
+
+func _on_detection_zone_body_entered(body: Node):
+	var clown = body as Clown
+	if not clown:
+		return
+	adjacent_clowns.append(clown)
+
+func _on_detection_zone_body_exited(body):
+	var clown = body as Clown
+	if not clown:
+		return
+	adjacent_clowns.erase(clown)
