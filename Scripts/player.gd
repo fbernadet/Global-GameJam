f4e7b31e98abf3498682c071cafb7a64d31a870b
JulienRAMIZ <j.ramiz@estia.fr>
2024-01-27 14:43:51 +0100

    Animation terminé

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,6 @@
 class_name Player
 extends CharacterBody2D
-
+@onready var _animated_sprite = $AnimatedSprite2D
 const speed = 100.0
 const friction = 0.35
 
@@ -19,3 +19,16 @@ func _physics_process(_delta):
 func _input(event):
 	if event.is_action_pressed("special"):
 		print("YES")
+
+func _process(_delta):
+	if Input.is_action_pressed("ui_up"):
+		_animated_sprite.play("run_arriere")
+
+	if Input.is_action_pressed("ui_down"):
+		_animated_sprite.play("run_avant")
+		
+	if Input.is_action_pressed("ui_left") || Input.is_action_pressed("ui_right"):
+		_animated_sprite.play()
+		
+	if Input.is_action_pressed("ui_left") == false && Input.is_action_pressed("ui_right") == false && Input.is_action_pressed("ui_up") == false && Input.is_action_pressed("ui_down") == false:
+		_animated_sprite.stop()
